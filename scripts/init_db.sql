18c3499a54bee5221487d98633fdcdd725d8f620
élise <102250391+elboyer228@users.noreply.github.com>
2026-01-23 12:43:00 +0100

    Add warnings

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,4 +1,8 @@
+-- Create main database and schemas bronze, silver, and gold
+-- WARNING: This script will drop the database "datawarehouse" if it exists 
 -- Must be run while connected to postgres 
+
+
 DROP DATABASE IF EXISTS datawarehouse;
 CREATE DATABASE datawarehouse;
 
